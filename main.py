6ce66ea2e501587646ffed73a829fffbbab67860
Aboodai9 <aboodahmedai09@gmail.com>
2025-07-16 14:57:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "Lz1aOcyTOC8PeKOQ8rmZGRyUHWaLuJhp","iqZFiYZi6Jn1aCnPNezP0OUVQtj9jnm1"]
+        "J0bcivbA2WO3x9ws0dFw8L8cbCRzHFZg","8Ok6nwIzexnSgxsFvDKDmiJmdmQ8vzQ4"]
     #ad ="https://www.effectiveratecpm.com/ca83bzpx98?key=dee9c6f3171b614287718132222041ad"
     #ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
     ad ="https://www.profitableratecpm.com/mrrsnt73cz?key=a9264f3ab1770607dd37951c2da5963c"
     #ad ="https://www.profitableratecpm.com/bygzdn8ti5?key=ac2134fc257fcbc83800d460291ac1ab"
+    ad ="https://viinnqwx.com/dc/?blockID=379919"
     for api in apis:
         for _ in range(randint (3,5)):
-            url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=fr&render_js=1"
+            url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
             urls.append(url)
 
     async with aiohttp.ClientSession() as session:
