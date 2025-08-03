5923d95beadc447e7067d106c83d5973e588075a
ll9pm <aboodll9pm@gmail.com>
2025-08-03 14:37:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def main():
     #ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
     ad ="https://www.profitableratecpm.com/mrrsnt73cz?key=a9264f3ab1770607dd37951c2da5963c"
     #ad ="https://www.profitableratecpm.com/bygzdn8ti5?key=ac2134fc257fcbc83800d460291ac1ab"
-    ad ="https://viinnqwx.com/dc/?blockID=381510"
+    ad ="https://viinnqwx.com/dc/?blockID=381701"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
