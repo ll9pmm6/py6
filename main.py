cca50f5a43102b61dddff474cbc9fd4d1f8b0396
ll9pm <aboodll9pm@gmail.com>
2025-08-07 14:04:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ async def main():
     """Creates an aiohttp session and fetches multiple URLs concurrently."""
     urls = list()
     apis =[
-        "flL3NyJh7AUQq3kRZyuYv2QqNOW6Bpw9","JMMYTedH3g383KlKWDUh1coJBSlxGzam"]
+        "IhDu0jgGtYMZb8aAsGn8PS83ib7iRMNH","cPVRrHFBAww2aSh3oCYxvBOljSCbDrjm"]
     #ad ="https://www.effectiveratecpm.com/ca83bzpx98?key=dee9c6f3171b614287718132222041ad"
     #ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
     ad ="https://www.profitableratecpm.com/mrrsnt73cz?key=a9264f3ab1770607dd37951c2da5963c"
     #ad ="https://www.profitableratecpm.com/bygzdn8ti5?key=ac2134fc257fcbc83800d460291ac1ab"
     ad ="https://viinnqwx.com/dc/?blockID=381701"
+    ad="https://www.profitableratecpm.com/es8iaffr0n?key=a180891d7e00848a91909a7b8081d758"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
