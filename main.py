b77ab259da6e1f1eca9acb69bd7736de5513fc55
ll9pm <aboodll9pm@gmail.com>
2025-08-16 14:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""print("start1")
+print("start1")
 import asyncio
 import aiohttp  # Requires installing: pip install aiohttp
 from random import randint
@@ -46,77 +46,5 @@ async def main():
 if __name__ == "__main__":
    for i in range (1000):
      print(i)
-     asyncio.run(main())"""
+     asyncio.run(main())
 
-import asyncio
-import httpx
-import time
-token = "sk_OIWw3O1JOZSg91rvK41nLLPXKxePYO09FTcSJt4GSPLn1y0ic0L4c9TBywdstds1"
-url="https://www.profitableratecpm.com/ziz6kzsc0s?key=ca1c1f39a7636892a713e4f321013c96"
-payload = {
-    "actor": "unlocker.webunlocker",
-    "proxy": {
-        "country": "IL"
-    },
-    "input": {
-        "url": url,
-        "jsRender": {
-            "enabled": True,
-            "waitUntil":"domcontentloaded",
-            "instructions":[{"wait": 7000}],
-            "response": {
-                "type": "html",  # png or jpeg
-            }
-        },
-    }
-}
-
-
-async def fetch_url(client, url):
-    """
-    Asynchronously fetches a single URL and returns its JSON content.
-    """
-    try:
-        # The 'await' keyword pauses this task until the response is received.
-        response = await client.post( "https://api.scrapeless.com/api/v2/unlocker/request",
-	    json=payload,
-	    headers={
-	        "x-api-token": token,
-	        "Content-Type": "application/json"
-	    },
-	    timeout=60)
-        # Raise an exception for bad status codes (4xx or 5xx).
-        
-        return response.json()
-    except httpx.HTTPError as exc:
-        return f"An HTTP error occurred for {exc.request.url!r}."
-    except Exception as exc:
-        return f"An unexpected error occurred for {url}: {exc}"
-
-async def main():
-    url="https://api.scrapeless.com/api/v2/unlocker/request"
-    """
-    Main asynchronous function to run all requests concurrently.
-    """
-    print("Starting 5 concurrent requests with httpx...")
-    start_time = time.time()
-
-    # Create a single AsyncClient session to share across all requests.
-    async with httpx.AsyncClient() as client:
-        # Create a list of 'tasks' to be run concurrently.
-        # Each task is an awaitable object returned by fetch_url.
-        tasks = [fetch_url(client, url) for _ in range(5)]
-
-        # Use asyncio.gather() to run all tasks at once.
-        # The results will be returned in the order the tasks were created.
-        results = await asyncio.gather(*tasks)
-
-    end_time = time.time()
-    print("All requests completed.")
-    print(f"Total time taken: {end_time - start_time:.2f} seconds.")
-    
-  
-if __name__ == "__main__":
-    for i in range(100):
-      asyncio.run(main())
-      print(i)
