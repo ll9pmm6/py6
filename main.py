9682684afb8aac4f7c1a3c1aa77145ac5f2614f2
ll9pmm6 <ame.rbotaimm@gmail.com>
2025-09-07 22:17:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def main():
     urls = list()
     apis =[
         "UWUhD0GWP6SLBDvsnHliqq3khznlFyiH","X9uAUVs7ywnGZ1egkqU82XQKWkfaJmue"]
-    ad="https://viidedss.com/dc/?blockID=388607"
+    ad="https://viidedss.com/dc/?blockID=388817"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
